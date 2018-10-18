9fdea500b5e143cbc8987cbbc912b52570531954
Joanna Erd <joanna.erd@elpassion.pl>
2018-10-18 19:07:08 +0200

    Add test fallback

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = () => {
-  const env = dotenv.config().parsed;
+  const env = dotenv.config().parsed || {};
   const envKeys = Object.keys(env).reduce((prev, next) => {
     prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
