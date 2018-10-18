439c4dee011c3aff660835dd2d3d379a052061eb
Joanna Erd <joanna.erd@elpassion.pl>
2018-10-18 13:27:20 +0200

    Add slack integration

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+
 const http = require('http');
 const socketIO = require('socket.io');
+const { WebClient } = require('@slack/client');
+const moment = require('moment');
 
+const CHANNEL_ID = process.env.SLACK_CHANNEL_ID;
+const SLACK_TOKEN = process.env.SLACK_TOKEN;
+
+const slackClient = new WebClient(SLACK_TOKEN);
 const app = http.createServer();
 const io = socketIO(app);
 
@@ -9,10 +17,16 @@ app.listen(8000, () => {
 });
 
 io.on('connection', socket => {
-  console.log('client connected,', socket.id);
-  socket.send({ body: 'hello!', sender: 'server' });
+  const onSummon = () => {
+    slackClient.chat.postMessage({
+      channel: CHANNEL_ID,
+      text: `It's ${moment(Date.now()).format('HH:mm:ss')} and someone's at the door!`
+    })
+      .then(() => {
+        socket.send({ body: 'Message sent to channel!' });
+      })
+      .catch(console.error);
+  };
 
-  socket.on('disconnect', () => console.log('client disconnected,', socket.id));
-
-  socket.on('summonClick', () => console.log('someone was summoned'));
+  socket.on('summonClick', onSummon);
 });
