cac0c1a80524a4db418589f8ccf79b34b930c0cc
Joanna Erd <joanna.erd@elpassion.pl>
2018-10-23 15:42:36 +0200

    Add interactive massage management

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const http = require('http');
+const parseBody = require('parse-body');
 const socketIO = require('socket.io');
 const { WebClient } = require('@slack/client');
 const moment = require('moment');
@@ -9,8 +10,32 @@ const PORT = process.env.PORT;
 const CHANNEL_ID = process.env.SLACK_CHANNEL_ID;
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
 
+const nowForHumans = () => moment(Date.now()).format('HH:mm:ss');
+
 const slackClient = new WebClient(SLACK_TOKEN);
-const app = http.createServer();
+const app = http.createServer(
+  (req, res) => {
+    if (req && req.method === 'POST') {
+      parseBody(req, 1e6, function (err, body) {
+        if (err) {
+          return console.error(err);
+        }
+
+        const { original_message: originalMessage, user, actions } = JSON.parse(body.payload);
+
+        io.to(actions[0].name).emit('message', 'slack response');
+
+        const response = {
+          ...originalMessage,
+          attachments: [{ 'text': `${user.name} is on their way (at ${ nowForHumans() })` }],
+        };
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(response));
+      });
+    }
+  }
+);
 const io = socketIO(app);
 
 app.listen(PORT, () => {
@@ -18,15 +43,34 @@ app.listen(PORT, () => {
 });
 
 io.on('connection', socket => {
-  console.log('connection')
+  const {id} = socket;
+  console.log(`incoming connection from ${id}`);
+
   const onSummon = () => {
-    console.log('summon')
+    console.log(`summon by ${id}`);
     slackClient.chat.postMessage({
       channel: CHANNEL_ID,
-      text: `It's ${moment(Date.now()).format('HH:mm:ss')} and someone's at the door!`
+      text: `It's ${nowForHumans()} and someone's at the door!`,
+      attachments: [
+        {
+          'text': 'Your reaction:',
+          'callback_id': 'summon',
+          'color': '#28c23e',
+          'attachment_type': 'default',
+          'actions': [
+            {
+              'name': socket.id,
+              'text': 'I\'m coming!',
+              'type': 'button',
+              'value': 'accepted'
+            }
+          ]
+        }
+      ]
     })
       .then(() => {
-        socket.send({ body: 'Message sent to channel!' });
+        console.log(`message sent to ${CHANNEL_ID}`);
+        socket.emit('message', 'Message sent to the channel!');
       })
       .catch(console.error);
   };
